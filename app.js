fd1111eb8d8f8e27669318f79da4db4c9287f430
tubaleviao <alvaro.prata.filho@gmail.com>
2022-03-12 22:32:59 +0000

    updating packages, current ip, default port

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express')
 const http = require('http')
 const upio = require('up.io')
 const socketIo = require('socket.io')
+var ip = require("ip");
 
 const router = require('./routes')
 const ioCode = require('./io')
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const app = express()
 const server = http.createServer(app)
 const io = socketIo(server)
@@ -19,4 +20,4 @@ app.set('view engine', 'ejs')
 app.use(router)
 io.on("connection", ioCode.con)
 
-server.listen(port, () => console.log(`Listening on port ${port}!`))
+server.listen(port, () => console.log(`Listening ${ip.address()}:${port}!`))
